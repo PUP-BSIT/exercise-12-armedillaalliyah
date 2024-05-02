287ce5d473f1a22cc4122d73542b666090cdb285
armedillaalliyah <armedillaalliyah@gmail.com>
2024-05-02 22:17:37 +0800

    update index.html

--- script.js
+++ script.js
@@ -1,41 +1,51 @@
 let count = 0;
 let intervalId;
 
-let startButton = document.getElementById("start");
-let pauseButton = document.getElementById("pause");
-let resumeButton = document.getElementById("resume");
-let stopButton = document.getElementById("stop");
-let countSpan = document.getElementById("count");
+const startButton = document.querySelector("#start");
+const pauseButton = document.querySelector("#pause");
+const resumeButton = document.querySelector("#resume");
+const stopButton = document.querySelector("#stop");
+const countSpan = document.querySelector("#count");
 
-startButton.addEventListener("click", function() {
-    intervalId = setInterval(function() {
-        count++;
-        countSpan.textContent = count;
-    }, 1000);
-    startButton.disabled = true;
-    pauseButton.disabled = false;
-});
+function updateCount() {
+  countSpan.textContent = count + " second/s";
+}
 
-pauseButton.addEventListener("click", function() {
-    clearInterval(intervalId);
-    pauseButton.disabled = true;
-    resumeButton.disabled = false;
-});
+function updateTimer() {
+  count++;
+  updateCount();
+}
 
-resumeButton.addEventListener("click", function() {
-    intervalId = setInterval(function() {
-        count++;
-        countSpan.textContent = count;
-    }, 1000);
-    pauseButton.disabled = false;
-    resumeButton.disabled = true;
-});
+function startTimer() {
+  intervalId = setInterval(updateTimer, 1000);
+  startButton.disabled = true;
+  pauseButton.disabled = false;
+}
 
-stopButton.addEventListener("click", function() {
-    clearInterval(intervalId);
-    count = 0;
-    countSpan.textContent = count;
-    startButton.disabled = false;
-    pauseButton.disabled = true;
-    resumeButton.disabled = true;
-});
+function resumeTimer() {
+  intervalId = setInterval(updateTimer, 1000);
+  pauseButton.disabled = false;
+  resumeButton.disabled = true;
+}
+
+function pauseTimer() {
+  clearInterval(intervalId);
+  pauseButton.disabled = true;
+  resumeButton.disabled = false;
+}
+
+function stopTimer() {
+  clearInterval(intervalId);
+  count = 0;
+  updateCount();
+  startButton.disabled = false;
+  pauseButton.disabled = true;
+  resumeButton.disabled = true;
+}
+
+startButton.addEventListener("click", startTimer);
+pauseButton.addEventListener("click", pauseTimer);
+resumeButton.addEventListener("click", resumeTimer);
+stopButton.addEventListener("click", stopTimer);
+
+updateCount();
